2b51485e9d5a8c046b6e2ceca16dd80bd44f4488
TommoBosha <116563699+TommoBosha@users.noreply.github.com>
2023-08-28 16:41:29 +0300

    update phone_regexp

--- utils/index.js
+++ utils/index.js
@@ -2,7 +2,7 @@ const UPLOAD_FILE_LIMIT_IN_BYTE = 3145728;
 
 const ALLOWED_FORMATS = ["jpg", "png", "webp"];
 
-const PHONE_REGEXP = /^\+380\d{9}$|^\+380\d{2}\s?\d{3}\s?\d{2}\s?\d{2}$/;
+const PHONE_REGEXP = /^\+380\d{9}$/;
 
 const PHONE_REGEXP_PARTNER = /^\+\d{1,3}\s?\d{2,3}\s?\d{3}\s?\d{2}\s?\d{2}$|^\+\d{10,12}$/;
 const EMAIL_REGEXP = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
